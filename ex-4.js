3871fdb19564850c735901b59093b8c56eba2162
FrankCW <wongasa999@gmail.com>
2024-09-12 03:25:58 +0700

    functions ex1-6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,25 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: function(a, b) {
+        return a + b; 
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return  a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+};
+
+let a = 10
+let b = 20
+let addResult = calculator.add(a, b);
+console.log(addResult)
+
+let aForDivide = 3000;
+let bForDivide = 10;
+let divideResult = calculator.divide(aForDivide, bForDivide);
+console.log(divideResult)
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,15 @@
 // Start coding here
+function findMinNumber(a, b) {
+    return Math.min(a, b);
+}
 const minNumber = findMinNumber(10, 20);
 
-console.log(minNumber); // 10
+console.log(Math.min(minNumber));
+
+console.log(Math.min(10, 20));
+
+console.log(Math.min(10, 20));
+
+const array1 = [10 ,20];
+
+console.log(Math.min(...array1));
